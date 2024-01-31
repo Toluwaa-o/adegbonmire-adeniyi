884ab74031cc20af0eaba7147d302c09919f3ca2
Toluwalashe <asheogunleye02@gmail.com>
2024-01-31 20:35:51 +0100

    fixed

--- app/Components/Header/Navbar.js
+++ app/Components/Header/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
     <nav>
       <HamburgerMenu showNavBar={show} clickHandler={setShow} />
       {show && (
-        <ul className="absolute h-[100vh] w-[100vw] top-0 left-0 bg-accent-white grid place-content-center nav_animation text-3xl gap-8 text-darkest-blue">
+        <ul className="fixed h-[100vh] w-[100vw] top-0 left-0 bg-accent-white grid place-content-center nav_animation text-3xl gap-8 text-darkest-blue z-20">
           {data.map((item) => (
             <LinkItem key={item.page} {...item} pname={pname} />
           ))}
